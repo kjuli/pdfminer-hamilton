c17931890613257a58f94d746952fe08727a21aa
Julijan Katic <julijan@katic.io>
2022-09-01 14:40:38 +0200

    added rijndael decryptor test

--- tests/test_rijndael.py
+++ tests/test_rijndael.py
@@ -1,7 +1,18 @@
 import unittest
 
-from pdfminer.rijndael import RijndaelEncryptor
+from pdfminer.rijndael import RijndaelEncryptor, RijndaelDecryptor
 
+
+class TestRijndaelDecryptor(unittest.TestCase):
+
+    key = bytes.fromhex('00010203050607080a0b0c0d0f101112')
+    ciphertext = bytes.fromhex('d8f532538289ef7d06b506a4fd5be9c9')
+    plaintext = '506812a45f08c889b97f5980038b8359'
+
+    def test_encryption(self):
+        decryptor = RijndaelDecryptor(self.key, 128)
+        assert self.encrypted == decryptor.encrypt(self.plaintext).hex()
+    
 
 class TestRijndaelEncryptor(unittest.TestCase):
 
