99b1477f29f7344749960354e56f98b31a83f4b7
Daniel Westberg <daniel.ar.westberg@gmail.com>
2022-09-02 11:37:26 +0200

    Updated arcfour.py and created unittests
    Co-authored-by: acke756 <acke756@users.noreply.github.com>

---
+++ tests/test_arcfour.py
@@ -0,0 +1,19 @@
+import unittest
+from pdfminer.arcfour import Arcfour
+#  Simplistic Test cases
+
+
+class TestArcfour(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(Arcfour(b'Key').process(
+            b'Plaintext').hex(), 'bbf316e8d940af0ad3')
+        self.assertEqual(Arcfour(b'Wiki').process(
+            b'pedia').hex(), '1021bf0420')
+        self.assertEqual(Arcfour(b'Secret').process(
+            b'Attack at dawn').hex(), '45a01f645fc35b383552544b9bf5')
+        return
+
+
+if __name__ == "__main__":
+    unittest.main()
