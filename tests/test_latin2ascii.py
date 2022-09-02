01ccfe475dbd2d8f3ea51269821d46dee81d929c
Daniel Westberg <daniel.ar.westberg@gmail.com>
2022-09-02 12:18:04 +0200

    Task 2: Updated latin2ascii.py and created unittest
    Co-authored-by: acke756 <acke756@users.noreply.github.com>

---
+++ tests/test_latin2ascii.py
@@ -0,0 +1,19 @@
+import contextlib
+import io
+import unittest
+from tools.latin2ascii import main
+
+
+class TestLatin2ascii(unittest.TestCase):
+
+    def test_main(self):
+        expected = "Thi^s i^s a te^xt with spe:ci:al chara:cto:rs"
+        with contextlib.redirect_stdout(io.StringIO()) as f:
+            main(argv=["", "samples/test_samples/sample_latin2ascii.txt"])
+        actual = f.getvalue()
+        self.assertEqual(expected, actual)
+        return
+
+
+if __name__ == "__main__":
+    unittest.main()
