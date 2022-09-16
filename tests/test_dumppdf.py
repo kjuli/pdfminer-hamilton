abff1b3edb1957f5b59753d7df0d7e9c79803025
Axel <acke756@gmail.com>
2022-09-16 20:57:17 +0200

    Wrote incomplete test for checking output of dumppdf.dumpoutline

--- tests/test_dumppdf.py
+++ tests/test_dumppdf.py
@@ -1,8 +1,21 @@
 import unittest
+import io
+import sys
+
+from tools.dumppdf import dumpoutline
 
 
 class TestDumppdf(unittest.TestCase):
-    def test_fail(self):
+    def test_simple1(self):
+        sample_path = "samples/simple1.pdf"
+
+        with io.StringIO() as dump_output:
+            dumpoutline(dump_output, sample_path, [], set())
+            dump_output.seek(0)
+
+            for line in dump_output:
+                sys.stdout.write(line)
+
         self.assertTrue(False)
 
 
