1f3a4387bba0d7da83b1b4ce3e1c5895477fdf91
Axel <acke756@gmail.com>
2022-09-16 21:11:46 +0200

    Extracted method compare_outlines from test_simple1

--- tests/test_dumppdf.py
+++ tests/test_dumppdf.py
@@ -5,18 +5,19 @@ from tools.dumppdf import dumpoutline
 
 
 class TestDumppdf(unittest.TestCase):
-    def test_simple1(self):
-        sample_path = "samples/simple1.pdf"
-
+    def compare_outlines(self, sample, expected):
         with io.StringIO() as dump_output:
-            dumpoutline(dump_output, sample_path, [], set())
+            dumpoutline(dump_output, sample, [], set())
             dump_output.seek(0)
             dump_output_lines = dump_output.readlines()
 
-            with open("samples/test_samples/simple1_outline.txt") as expect_file:
+            with open(expected) as expect_file:
                 expected_lines = expect_file.readlines()
 
                 self.assertEqual(dump_output_lines, expected_lines)
+
+    def test_simple1(self):
+        self.compare_outlines("samples/simple1.pdf", "samples/test_samples/simple1_outline.txt")
 
 
 if __name__ == "__main__":
