2321d2ebb87fb35b7eff3aa19dbe12ce4875d62d
Axel <acke756@gmail.com>
2022-09-17 17:29:31 +0200

    Added test for OutlineList.as_list

--- tests/test_dumppdf.py
+++ tests/test_dumppdf.py
@@ -1,22 +1,26 @@
 import unittest
 import io
 
-from tools.dumppdf import dumpoutline_legacy, dumpoutline_new
+from tools.dumppdf import dumpoutline_legacy, dumpoutline_new, OutlineList
+from pdfminer.pdfdocument import PDFDocument, PDFNoOutlines
+from pdfminer.pdfparser import PDFParser
+
+
+# sample_outline_pairs contains tuples (pdf, txt) such that
+# txt contains the expected output of running dumpoutline on pdf
+sample_outline_pairs = [(
+    "samples/simple1.pdf",
+    "samples/test_samples/simple1_outline.txt"
+), (
+    "samples/test_samples/Introduction_To_Modern_Cryptography.pdf",
+    "samples/test_samples/Introduction_To_Modern_Cryptography_outline.txt"
+), (
+    "samples/nonfree/dmca.pdf",
+    "samples/test_samples/dmca_outline.txt"
+)]
 
 
 class TestDumpoutline(unittest.TestCase):
-    # sample_outline_pairs contains tuples (pdf, txt) such that
-    # txt contains the expected output of running dumpoutline on pdf
-    sample_outline_pairs = [(
-        "samples/simple1.pdf",
-        "samples/test_samples/simple1_outline.txt"
-    ), (
-        "samples/test_samples/Introduction_To_Modern_Cryptography.pdf",
-        "samples/test_samples/Introduction_To_Modern_Cryptography_outline.txt"
-    ), (
-        "samples/nonfree/dmca.pdf",
-        "samples/test_samples/dmca_outline.txt"
-    )]
 
     """Asserts that the content of the file with filepath expected is equal to
     the result of running dumpoutline on the .pdf file with filepath sample."""
@@ -32,12 +36,32 @@ class TestDumpoutline(unittest.TestCase):
                 self.assertEqual(dump_output_lines, expected_lines)
 
     def test_sample_outline_pairs_legacy(self):
-        for (sample, expected) in self.sample_outline_pairs:
+        for (sample, expected) in sample_outline_pairs:
             self.compare_outlines(sample, expected, dumpoutline_legacy)
 
     def test_sample_outline_pairs_classed(self):
-        for (sample, expected) in self.sample_outline_pairs:
+        for (sample, expected) in sample_outline_pairs:
             self.compare_outlines(sample, expected, dumpoutline_new)
+
+
+class TestOutlineList(unittest.TestCase):
+    def test_outlines_as_list(self):
+        for (sample, expected) in sample_outline_pairs:
+            with open(sample, "rb") as fp:
+                parser = PDFParser(fp)
+
+                try:
+                    outlines = OutlineList(PDFDocument(parser, b""))
+                    outline_list = outlines.as_list()
+                    i = 0
+
+                    for elem in outlines:
+                        self.assertEqual(elem, outline_list[i])
+                        i += 1
+                except PDFNoOutlines:
+                    pass
+
+                parser.close()
 
 
 if __name__ == "__main__":
